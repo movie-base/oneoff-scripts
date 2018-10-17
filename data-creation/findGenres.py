e5be8faf131a1d2c1d38585cd7968592904658b1
Jason <jason>
2018-10-17 23:57:48 +1100

    Split based on '*' instead of '-' and skip header

--- data-creation/findGenres.py
+++ data-creation/findGenres.py
@@ -3,22 +3,23 @@
 	for each genre. It returns a list of genres, sorted in descending frequency.
 	More specifically, the function returns:
 	[
-		('Drama', 15467), ('Comedy', 10394), ('Romance', 4995), ('Action', 4434),
-		('Crime', 4303), ('Thriller', 4001), ('Documentary', 3391), ('Horror', 3363),
-		('Adventure', 3217), ('Mystery', 1992), ('Fi', 1791), ('Sci', 1791), ('Family', 1770),
-		('Biography', 1678), ('Fantasy', 1670), ('Animation', 1387), ('History', 1176),
-		('Short', 1155), ('Music', 1123), ('War', 973), ('Western', 765), ('Musical', 737),
-		('Sport', 610), ('Noir', 354), ('Film', 354), ('News', 73), ('Adult', 24),
-		('Show', 24), ('TV', 15), ('Reality', 15), ('Talk', 13), ('Game', 11),
+		('Drama', 15467), ('Comedy', 10394), ('Romance', 4995), ('Action', 4434), ('Crime', 4303),
+		('Thriller', 4001), ('Documentary', 3391), ('Horror', 3363), ('Adventure', 3217), ('Mystery', 1992),
+		('Sci-Fi', 1791), ('Family', 1770), ('Biography', 1678), ('Fantasy', 1670), ('Animation', 1387),
+		('History', 1176), ('Short', 1155), ('Music', 1123), ('War', 973), ('Western', 765), ('Musical', 737),
+		('Sport', 610), ('Film-Noir', 354), ('News', 73), ('Adult', 24), ('Reality-TV', 15), ('Talk-Show', 13),
+		('Game-Show', 11),
 	]
 """
 
 def findGenres(csvFilename):
 	moviesFile = open(csvFilename, "r")
 	genreFrequency = {}
+
+	moviesFile.readline()			# Skips header.
 	for line in moviesFile:
 		id, genres, rating = line.split(",")
-		genreList = genres.split("-")
+		genreList = genres.split("*")
 		for genre in genreList:
 			if genre != "None":
 				if genre not in genreFrequency:
